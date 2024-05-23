393f3315d3f693cd343163aa091ff422fa8a0194
CheezyLeezy1 <x20115075@student.ncirl.ie>
2024-05-23 21:04:53 +0100

    - Removed all Module Files
    Completed-By:
    Lee Campbell

---
+++ components/copy-to-clipboard.tsx
@@ -0,0 +1,27 @@
+const Button = (message: string) => {
+  return (
+    <button
+      className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
+      data-state="closed"
+    >
+      Copy to Clipboard{message}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="ml-2 h-4 w-4"
+      >
+        <rect width="8" height="4" x="8" y="2" rx="1" ry="1"></rect>
+        <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+      </svg>
+    </button>
+  )
+}
+
+export default Button

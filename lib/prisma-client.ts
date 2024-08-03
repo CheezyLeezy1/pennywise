7631a56b0372fafe83c7d519af5574de25c5098c
CheezyLeezy1 <x20115075@student.ncirl.ie>
2024-08-03 02:59:19 +0100

    [Feature Branch]:
    - prettier fix
    
    Completed-By:
    Lee Campbell

--- lib/prisma-client.ts
+++ lib/prisma-client.ts
@@ -23,7 +23,7 @@ export const prisma = new PrismaClient({
   ],
 })
 
-prisma.$on('query', (e:any) => {
+prisma.$on('query', (e: any) => {
   console.log('Query: ' + e.query)
   console.log('Params: ' + e.params)
   console.log('Duration: ' + e.duration + 'ms')
